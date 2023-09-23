62b4b39095d701e81f5278decd8cfe3c875f32c9
유해니 <haeni311@gmail.com>
2023-09-24 08:56:56 +0900

    어노테이션 수정

--- src/main/java/com/heuron/backend/patient/dto/PatientsUpdateDto.java
+++ src/main/java/com/heuron/backend/patient/dto/PatientsUpdateDto.java
@@ -2,10 +2,11 @@ package com.heuron.backend.patient.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-@Data
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
