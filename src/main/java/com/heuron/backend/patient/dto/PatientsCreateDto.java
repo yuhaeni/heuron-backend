ba79a64ac02f2a382c7eacb4827812c9d019192f
유해니 <haeni311@gmail.com>
2023-09-24 20:13:56 +0900

    환자 생성 DTO - 유효성 검사 추가

--- src/main/java/com/heuron/backend/patient/dto/PatientsCreateDto.java
+++ src/main/java/com/heuron/backend/patient/dto/PatientsCreateDto.java
@@ -1,24 +1,40 @@
 package com.heuron.backend.patient.dto;
 
 import com.heuron.backend.patient.domain.Patients;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.util.StringUtils;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.sql.Timestamp;
+import java.util.Objects;
 
 @Getter
+@AllArgsConstructor
 @Builder
 public class PatientsCreateDto {
+
+
+    @NotBlank(message = "Name is required.")
     private String name;
+
+    @NotNull(message = "Age is required.")
     private int age;
+
+    @Pattern(regexp = "^[MW]$", message = "Invalid gender. Should be 'M' or 'W'.")
+    @NotBlank(message = "Gender is required.")
     private String gender;
+
+    @Pattern(regexp = "^[YN]$", message = "Invalid disease flag. Should be 'Y' or 'N'.")
+    @NotBlank(message = "Disease flag is required.")
     private String diseaseFlag;
 
-    public PatientsCreateDto(String name, int age, String gender, String diseaseFlag) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.diseaseFlag = diseaseFlag;
-    }
 
     public Patients toEntity() {
         return Patients.builder()
